40e4567b7c3f7bc71d1015d2c0cc789540e0f9e7
StevenReale <112089327+StevenReale@users.noreply.github.com>
2022-09-04 14:10:10 -0400

    Add files via upload

--- sum-of-primes.py
+++ sum-of-primes.py
@@ -1,20 +1,22 @@
+import math
 #sum of all primes less than input
 input = 2000000
 
 #initializes variables for main while loop
-primelist = [2]
-primeTotal = 2
-startTest = 3
+primelist = [2, 3]
+primeTotal = 5
+startTest = 4
 print("2 is prime")
 
-#attempts to divide test value by all values currently on primelist
+#attempts to divide test value by all values currently on primelist that are no larger than test didvided by the square root of the previously found prime
 def isPrime(primelist, test):
     span = len(primelist)
     for i in range(span):
+        if primelist[i] > test/math.sqrt(primelist[i]):
+            print(test, "is prime")
+            return True
         if test % primelist[i] == 0:
             return False
-    print(test, "is prime")
-    return True
 
 #calls isPrime to determine if value is prime. If it is, it adds it to primelist and to the running sum of primes
 while startTest < input:
